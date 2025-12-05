9b83d388fcf4e9f16c29c69b595acffacb96a858
Mark McClatchy <markmcclatchy@gmail.com>
2025-12-05 17:27:00 -0500

    Add complete formula with TestPyPI package and dependencies

--- Formula/respec-ai.rb
+++ Formula/respec-ai.rb
@@ -3,18 +3,37 @@ class RespecAi < Formula
 
   desc "AI-powered spec workflow automation (DEV BUILD - TestPyPI)"
   homepage "https://github.com/mmcclatchy/respec-ai"
-  url "https://test-files.pythonhosted.org/packages/source/r/respec-ai/respec-ai-0.4.7.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://test-files.pythonhosted.org/packages/d9/36/8c76042dadf20114088f4bd28de75f6cd1cebc99accf078f6e7fe28c149e/respec_ai-0.4.7.tar.gz"
+  sha256 "2382162472311f2f9d095007227b7fc44852cc19b9936401463eb77970229c8b"
   license "MIT"
 
   depends_on "python@3.11"
   depends_on "docker"
 
-  # Dependencies will be added via homebrew-pypi-poet
-  # resource "pydantic" do
-  #   url "..."
-  #   sha256 "..."
-  # end
+  resource "docker" do
+    url "https://files.pythonhosted.org/packages/91/9b/4a2ea29aeba62471211598dac5d96825bb49348fa07e906ea930394a83ce/docker-7.1.0.tar.gz"
+    sha256 "ad8c70e6e3f8926cb8a92619b832b4ea5299e2831c14284663184e200546fa6c"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+  end
+
+  resource "pydantic" do
+    url "https://files.pythonhosted.org/packages/69/44/36f1a6e523abc58ae5f928898e4aca2e0ea509b5aa6f6f392a5d882be928/pydantic-2.12.5.tar.gz"
+    sha256 "4d351024c75c0f085a9febbb665ce8c0c6ec5d30e903bdb6394b7ede26aebb49"
+  end
+
+  resource "pydantic-settings" do
+    url "https://files.pythonhosted.org/packages/43/4b/ac7e0aae12027748076d72a8764ff1c9d82ca75a7a52622e67ed3f765c54/pydantic_settings-2.12.0.tar.gz"
+    sha256 "005538ef951e3c2a68e1c08b292b5f2e71490def8589d4221b95dab00dafcfd0"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/fb/d2/8920e102050a0de7bfabeb4c4614a49248cf8d5d7a8d01885fbb24dc767a/rich-14.2.0.tar.gz"
+    sha256 "73ff50c7c0c1c77c8243079283f4edb376f0f6442433aecb8ce7e6d0b92d1fe4"
+  end
 
   def install
     virtualenv_install_with_resources
